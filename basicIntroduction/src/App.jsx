f0cd3e34dac059f58219188d3ce77555f8f7ce3d
atul <atulshivaan@gmail.com>
2024-09-14 09:43:12 +0530

    Initial commit of all files to the repository

---
+++ basicIntroduction/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+import HelloWord from './components/HelloWord'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <div className="card">
+        <HelloWord/>
+         </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ basicIntroduction/src/components/HelloWord.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./helloworld.css"
+const HelloWord = () => {
+  return (
+    <>
+    Hello world
+    
+    
+    </>
+  )
+}
+
+export default HelloWord
